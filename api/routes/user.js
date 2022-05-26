5277fc28e87498f9b8f95401b4b707eec124f662
safayet-arman <safayetarman11@gmail.com>
2022-05-26 21:00:57 +0600

    mongoDB connection

---
+++ api/routes/user.js
@@ -0,0 +1,14 @@
+const router = require("express").Router();
+
+router.get("/usertest", (req,res)=>{
+    res.send("wser test is successfull")
+});
+
+
+router.post("/usertest",(req,res)=>{
+    const username = req.body.username;
+    console.log(username);
+    res.send("your username is " + username);
+})
+
+module.exports = router;
